08fb722f6ae76df6a235d74a780c6b7a67368b9f
drewkb <drewkb@bk.ru>
2018-03-16 18:57:56 +0300

    simple template and view

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Post
 
-# Create your views here.
+
+def lst(request):
+    posts = Post.objects.all()
+    return render(request, 'blog/list.html', {'posts':posts})
